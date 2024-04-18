a6c4b316f068574fd3dc18fb6d52b117117d86e3
merasil <bastianhorn@gmail.com>
2024-04-18 16:56:34 +0200

    dsebug

--- rec_faces.py
+++ rec_faces.py
@@ -99,7 +99,7 @@ while True:
             continue
     img = stream.last_frame
     try:
-        faces = DeepFace.find(img_path=img, detector_backend=detector, db_path=path_db, distance_metric=metric, model_name=model, silent=debug)
+        faces = DeepFace.find(img_path=img, detector_backend=detector, db_path=path_db, distance_metric=metric, model_name=model, silent=False)
     except KeyboardInterrupt:
         print("Killing Process...")
         break
